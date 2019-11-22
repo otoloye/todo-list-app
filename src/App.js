03fa2d8a27ab11c93540d3e6c0c787dc8e6e7c90
Otoloye Oyeniran <otoloyeoyeniran@gmail.com>
2019-11-22 10:52:33 -0500

    update list attempt

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  const Todos = ['Buy Water', 'Eat Food', 'Look around'];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { Todos: ['Buy Water', 'Eat Food', 'Look around'] };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  return (
-    <div className="todo-container">
-      <ul className="todo-unordered-list">
-        {Todos.map(todo => (
-          <li>{todo}</li>
-        ))}
-      </ul>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter todo here"
-          className="todo-input"
-        />
-        <button type="submit" className="todo-button">
-          Add todo
-        </button>
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  handleChange(e) {
+    this.setState({ Todos: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="todo-container">
+        <ul className="todo-unordered-list">
+          {this.state.Todos.map(todo => (
+            <li>{todo}</li>
+          ))}
+        </ul>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter todo here"
+            className="todo-input"
+            value={this.state.Todos}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit} className="todo-button">
+            Add todo
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
